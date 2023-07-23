84a6ba380d71b4382f60a6e83f4911a7a9481fe1
mrtkrkrt <muratkrkrt0711@gmail.com>
2023-07-23 21:22:36 +0300

    minor

--- src/main/java/com/folksdev/workshop/dto/TodoDto.java
+++ src/main/java/com/folksdev/workshop/dto/TodoDto.java
@@ -1,10 +1,6 @@
 package com.folksdev.workshop.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
 
@@ -12,14 +8,7 @@ import java.util.Date;
 public class TodoDto {
     private Long id;
     private Long userId;
-
-    @Size(min = 5, max = 1000)
-    @NotBlank
     private String description;
-
-
     private boolean isComplete;
-
-    @CreationTimestamp
     private Date createdDate;
 }
